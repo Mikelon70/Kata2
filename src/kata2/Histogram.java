dc22fe0e8cfafbfa02949ff901de39f2ebaec709
migue <migue@DESKTOP-6DDSOAL>
2021-09-30 10:41:42 +0100

    Kata2 version 3

--- src/kata2/Histogram.java
+++ src/kata2/Histogram.java
@@ -5,23 +5,17 @@ import java.util.Map;
 
 public class Histogram {
     private final int[] data;
-    
+
     public Histogram(int[] array) {
         this.data = array;
     }
-    
+
     public int[] getData() {
         return this.data;
     }
-    
+
     public Map getHistogram() {
         Map<Integer, Integer> histogram = new HashMap<Integer, Integer>();
-        histogram.put(1, 4);
-        histogram.put(6, 2);
-        histogram.put(8, 1);
-        histogram.put(3, 0);
-        histogram.put(7, 9);
-        histogram.put(13, 11);
         for (int key : data) {
             histogram.put(key, histogram.containsKey(key) ? histogram.get(key) + 1 : 1);
         }
